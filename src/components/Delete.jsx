fe45633ca1330aef713ad83a1727fae4c64b8f18
nithyakalathil <nithya913k@gmail.com>
2024-06-08 12:14:42 +0530

    created

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -6,7 +6,7 @@ const Delete = () => {
     <div>
 <Navbar/>
 <div className="container">
-    <div className="row">
+    <div className="row g-3">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
         <label htmlFor="" className="form-label">Product Id</label>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,7 +6,7 @@ const Search = () => {
     <div>
 <Navbar/>
 <div className="container">
-    <div className="row">
+    <div className="row g-3">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
 
